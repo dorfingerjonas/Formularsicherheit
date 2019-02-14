8d28fbe140005b8392dd5f3bb09856a307a987f7
Joonnnaas <36422131+Joonnnaas@users.noreply.github.com>
2019-02-14 12:01:35 +0100

    email adress check works!

--- script/script.js
+++ script/script.js
@@ -1,20 +1,23 @@
 const checkNumber = /[0-9]/g;
+const checkMailUsername = /[^a-zA-Z0-9.]/g;
+const checkMailDomain = /[^a-z.]/g;
 
 function checkInputs() {
   checkFirstName();
   checkLastName();
   checkEmailAdress();
+  compareEmails();
+
+  console.log("\n");
 }
 
 function checkFirstName() {
   let firstname = document.getElementById('firstname').value;
 
-  let checkNumber = /[0-9]/g;
-
   if (checkNumber.test(firstname)) {
     printError();
   } else {
-    console.log(firstname);
+    console.log("Fisrtname: " + firstname);
   }
 }
 
@@ -24,31 +27,62 @@ function checkLastName() {
   if (checkNumber.test(lastname)) {
     printError();
   } else {
-    console.log(lastname);
+    console.log("Lastname: " + lastname);
   }
 }
 
-
 function checkEmailAdress() {
-  let mail = document.getElementById('emailAdress').value;
-  let parts = mail.split('@');
+  let mailAdress = document.getElementById('mailAdress').value;
+  let parts = mailAdress.split('@');
   let username = parts[0];
-  let provider = parts[1];
+  let domain = parts[1];
 
-  checkUsername();
-
+  if (checkUsername(username) && checkDomain(domain)) {
+      console.log("E-Mail: " + mailAdress);
+  }
 }
 
-function checkUsername() {
-  const checkUsername = /-/g;
-  let mail = document.getElementById('emailAdress').value;
-  let parts = mail.split('@');
-  let username = parts[0];
+function checkUsername(username) {
 
-  if (checkUsername.test(username) && !(checkNumber.test(username))) {
+  if (checkMailUsername.test(username)) {
     printError();
   } else {
-    console.log(username);
+    console.log("Username: " + username);
+    return true;
+  }
+}
+
+function checkDomain(domain) {
+
+  let parts = domain.split('.');
+  let provider = parts[0];
+  let topLevelDomain = parts[1];
+
+  if (checkMailDomain.test(domain) && istTLDValid(topLevelDomain)) {
+    printError();
+  } else {
+    console.log("Domain: " + domain);
+    return true;
+  }
+}
+
+function istTLDValid(topLevelDomain) {
+
+  if (topLevelDomain.length <= 3 && topLevelDomain.length >= 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function compareEmails() {
+  let mailAdress = document.getElementById('mailAdress').value;
+  let repeatMailAdress = document.getElementById('repeatMailAdress').value;
+
+  if (mailAdress === repeatMailAdress) {
+    console.log("E-Mail Adresses are equal");
+  } else {
+    printError();
   }
 }
 
